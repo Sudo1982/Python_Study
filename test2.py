85033d7b1c11bbdc8f9eddedc48fc4f3dfc8ec22
KentaroSudo <ke.sudo.us@hitachitransport.com>
2020-11-21 15:06:51 +0900

    test

---
+++ test2.py
@@ -0,0 +1,25 @@
+Python 3.7.3 (default, Jul 25 2020, 13:03:44) 
+[GCC 8.3.0] on linux
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> class orange:
+	def __init__(self,w,c):
+		self.weight = w
+		self.color =
+		print("Created")
+
+		
+>>> or1 = orange(4,'light orange')
+Created
+>>> or1 = orange(8,'dark orange')
+Created
+>>> or1 = orange(4,'light orange')
+Created
+>>> or2 = orange(8,'dark orange')
+Created
+>>> or3 = orange(14,'yellow')
+Created
+>>> or2.weight
+8
+>>> or3.color
+'yellow'
+>>> 
