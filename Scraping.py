f1d6365ffe486389142761756ea43a245149e1d3
KentaroSudo <ke.sudo.us@hitachitransport.com>
2020-11-21 19:29:17 +0900

    correct

--- Scraping.py
+++ Scraping.py
@@ -8,16 +8,10 @@ class Scraper:
 
     def scrape(self):
         r = urllib.request.urlopen(self.site)
-        html = r.read()
-        parser = "html.parser"
-        sp = BeautifulSoup(html,
-                           parser)
-        for tag in sp.find_all("a"):
-            url = tag.get("href")
-            if url is None:
-                continue
-            if "html" in url:
-                print("\n" + url)
+        soup = BeautifulSoup(r.read(), 'html.parser')
 
-news = "https://news.google.com/"
+        for tag in soup.find_all('a'):
+            print(tag.get('href'))
+
+news = 'https://news.google.com/'
 Scraper(news).scrape()
